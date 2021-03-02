4063288fcb0b65154d166553d0a14f200cf3e458
deemarc <deemarc.br@gmail.com>
2021-03-02 13:56:22 +0700

    add user register and login feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 import CartPage from './pages/CartPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomePage} exact />
+          <Route path='/login' component={LoginPage} exact />
           <Route path='/product/:id' component={ProductPage} />
           <Route path='/cart/:id?' component={CartPage} />
         </Container>
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-
-    const { data } = await axios.get(`/api/product/${id}`)
+    console.log("update cart:"+id+",qty:"+qty)
+    const { data } = await axios.get(`/api/products/${id}`)
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
